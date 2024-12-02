659c79c05df0752da45ef94a19ffdd87ac0783d2
sanjaii1 <bsanjay235@gmail.com>
2024-12-02 17:38:51 +0530

    setup into project base

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="container mt-5">
+      <h1>
+        hello world
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
