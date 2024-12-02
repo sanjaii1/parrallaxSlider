2d79569be7a953b9e047c7a033d95363bc637a09
sanjaii1 <bsanjay235@gmail.com>
2024-12-02 17:56:21 +0530

    init all files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/header";
+import Slider from "./components/slider";
+import NextPart from "./components/nextPart";
 
 const HomePage = () => {
   return (
-    <div className="container mt-5">
-      <h1>
-        hello world
-      </h1>
+    <div className="container" style={{ display: "flex", justifyContent: "center", alignItems: "center", }}>
+    <div style={{width:'100%'}}>
+      <Header />
+      <Slider />
+      <NextPart />
     </div>
+  </div>
+  
   );
 };
 
